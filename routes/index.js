ca756be82be0eee16366f72b1a09068986ace8ae
liupengke <pengkeliu@gmail.com>
2014-06-03 21:07:51 +0800

    s

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var auth = require('../controller/auth');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -10,6 +11,33 @@ router.get('/product', function(req, res) {
 });
 router.get('/product/id', function(req, res) {
   res.render('product', { title: 'Express' });
+});
+router.get('/admin', function(req, res){
+	var isValid = auth.checkLogin(req);
+	if(isValid)
+		res.render('admin/index');
+	else
+		res.render('admin/login');
+});
+router.get('/admin/login', function(req, res) {
+  res.render('admin/login', { title: 'Express' });
+});
+router.get('/admin/goods/:id?', function(req, res){
+	var isValid = auth.checkLogin(req);
+	if(isValid)
+		res.render('admin/goods');
+	else
+		res.render('admin/login');
+});
+router.post('/admin/login', function(req, res){
+	console.log(1)
+	if(!req.body.username || !req.body.password)
+		res.redirect('/admin/login');
+	console.log(2);
+	auth.login(req.body.username, req.body.password, req, function(){
+		console.log(3);
+		res.redirect('/admin');
+	});
 });
 
 
